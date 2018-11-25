734516112f83a282a63306ff9c01b0bc2447b261
FonFon <rdfonfon@gmail.com>
2018-11-25 14:17:35 +0300

    #4 - Handler & Runnable - возвращаем текст назад, если телефон не трясется

--- app/src/main/java/com/fonfon/magicball/MainActivity.kt
+++ app/src/main/java/com/fonfon/magicball/MainActivity.kt
@@ -3,6 +3,7 @@ package com.fonfon.magicball
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.Gravity
 import android.view.View
 import android.widget.TextView
@@ -16,6 +17,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var ui: MainActivityUI
     lateinit var shakeDetector: ShakeDetector
 
+    private val handler = Handler()
+    private val runnable = Runnable { ui.textView.text = "Потряси меня" }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ui = MainActivityUI()
@@ -23,16 +27,20 @@ class MainActivity : AppCompatActivity() {
 
         shakeDetector = ShakeDetector(this) {
             ui.textView.text = if (random.nextBoolean()) "да" else "нет"
+            handler.removeCallbacks(runnable)
+            handler.postDelayed(runnable, 1500)
         }
     }
 
     override fun onResume() {
         super.onResume()
+        ui.textView.text = "Потряси меня"
         shakeDetector.start()
     }
 
     override fun onPause() {
         super.onPause()
+        handler.removeCallbacks(runnable)
         shakeDetector.stop()
     }
 }
