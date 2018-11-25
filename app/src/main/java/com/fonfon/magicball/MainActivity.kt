9e0d156645368901828255fb902823876ee16792
FonFon <rdfonfon@gmail.com>
2018-11-25 13:41:53 +0300

    #2 - Простая верстка с помощью Kotin библиотеки Anko c использованием DSL(domain-specific language)

--- app/src/main/java/com/fonfon/magicball/MainActivity.kt
+++ app/src/main/java/com/fonfon/magicball/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.fonfon.magicball
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.TextView
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var ui: MainActivityUI
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ui = MainActivityUI()
+        ui.setContentView(this)
     }
+}
+
+//Класс, содержащий в себе верстку
+class MainActivityUI: AnkoComponent<MainActivity> {
+
+    //lateinit объявляет, что эта переменная никогда не будет пустой, но инициализируется она не сразу
+    lateinit var textView: TextView
+
+    //kotlin позволяет начиниать функции с =
+    override fun createView(ui: AnkoContext<MainActivity>): View = with(ui) {
+        frameLayout {
+            textView = textView {
+                text = "Потряси меня"
+                textSize = 64f
+                textColor = Color.WHITE
+                gravity = Gravity.CENTER
+            }.lparams {
+                gravity = Gravity.CENTER
+                margin = 16
+            }
+        }
+    }
+
 }
